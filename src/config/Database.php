6d816b77fd8bbe7967416745a51266c6a690d439
gabrielfpereira <gabrielf.pereira@outlook.com>
2020-06-24 18:43:25 -0300

    Criando classe de banco de dados

---
+++ src/config/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database
+{
+    public static function getConnection()
+    {
+        $envPatch = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPatch); // servem para referenciar o arquivo que contem as variaveis de ambiente
+
+        $conn = new mysqli($env['host'], $env['username'], $env['password'], $env['database']);
+
+        if ($conn->connect_error) {
+            die("Erro: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+}
