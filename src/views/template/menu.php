3d55be09b0330a27871420c3742a525989a82ae0
gabrielfpereira <gabrielf.pereira@outlook.com>
2020-06-28 15:53:24 -0300

    Criação menu lateral

---
+++ src/views/template/menu.php
@@ -0,0 +1,33 @@
+<aside class="sidebar">
+    <nav class="menu">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href="day_records.php">
+                    <i class="icofont-ui-check mr-2"></i>
+                    Registrar Ponto
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-ui-calendar mr-2"></i>
+                    Relatório Mensal
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-chart-histogram mr-2"></i>
+                    Relatório Geral
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-users mr-2"></i>
+                    Usuários
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
